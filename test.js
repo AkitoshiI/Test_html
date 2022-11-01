ff1fabe068b76d925d904612e064cde8f1f2f9a4
akitoshi <aki03194456@gmail.com>
2022-11-01 21:53:38 +0900

    699px以下でのハンガーメニュー実装

--- test.js
+++ test.js
@@ -1,15 +1,34 @@
-const eltag = document.querySelectorAll('a[href^="#"]');
-
-for(let i = 0; i<4; i++){
+//スクロールイベント
+const eltag = document.querySelectorAll('a[id]');
+elBtn = document.getElementById("nav-contener");
+for(let i = 0; i<6; i++){
 eltag[i].addEventListener('click' ,e=>{
     e.preventDefault;
-    console.log(document.querySelector(e.target.hash).getBoundingClientRect().top);
-    const pos = document.querySelector(e.target.hash).getBoundingClientRect().top; 
-    window.scrollTo({
-        top: pos,
-        left: 0,
-        behavior: 'smooth'
-      });
+    const elClass = document.querySelectorAll('a');
+    for(let j = 0; j<6; j++){
+      elClass[j].classList.remove('active')
+      console.log(elClass[j])
+    }
+    elBtn.classList.toggle('active');
+    elnav = document.querySelector('nav ul');
+    elnav.classList.toggle('active');
+    
+    document.getElementById(e.target.id).classList.add('active');
+    console.log(e);
+    //const posY =window.pageYOffset;
+    const pos = document.getElementById("_"+e.target.id).getBoundingClientRect().top; 
+    window.scrollBy({
+      top: pos-50,
+      left: 0,
+      behavior: 'smooth'
+    });
     return false;
 })
 }
+//ハンガーメニュー
+elBtn.addEventListener('click',e=>{
+  elBtn.classList.toggle('active');
+  elnav = document.querySelector('nav ul');
+  elnav.classList.toggle('active');
+}
+)
